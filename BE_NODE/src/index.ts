1d9448b92c1ef22e4c9f0af7fef6740f02c1f944
Ravim-addweb <Ravim-addweb>
2024-03-01 19:42:36 +0530

    merge

--- BE_NODE/src/index.ts
+++ BE_NODE/src/index.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import cors from 'cors'
-import route from "./routes";
+import express from 'express';
+import cors from 'cors';
+import route from './routes';
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 // Express configuration
 app.set('port', process.env.PORT || 5000);
 
